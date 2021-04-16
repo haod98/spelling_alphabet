2798477d295fa2aff573c5d5a468b5f949ca572f
Hao <74541600+haod98@users.noreply.github.com>
2021-04-16 18:37:22 +0200

    Change letter on input

--- main.js
+++ main.js
@@ -2,13 +2,51 @@ const input = document.querySelector('input[name="input_text"]');
 const words_container = document.querySelector('.container-words');
 const word_list = document.querySelector('.container-words');
 console.log(word_list);
+const english = {
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliett",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-ray",
+    "Y": "Yankee",
+    "Z": "Zulu",
+    " ": "---------------"
+};
+
+
+
 input.addEventListener('keyup', (e) => {
     text_value = e.target.value;
     word_list.innerHTML = '';
+    const div = document.createElement('div');
+    words_container.appendChild(div);
 
+    console.log(e);
     for (let i = 0; i < text_value.length; i++) {
         const word = document.createElement('p');
-        word.textContent = text_value.charAt(i);
-        words_container.appendChild(word)
+        const char = text_value.charAt(i)
+        word.textContent = `${char}- ${english[char.toUpperCase()]}`;
+        div.appendChild(word);
     };
+
 });
+
